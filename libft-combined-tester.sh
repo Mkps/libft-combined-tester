f5d279865a7cecb50238f9a5098079942d5ea7ed
Alexis Loubiere <alex.loubiere@gmail.com>
2022-12-11 19:17:26 +0100

    Updated script to create results directory

--- libft-combined-tester.sh
+++ libft-combined-tester.sh
@@ -6,12 +6,15 @@ RED="\033[31m"
 CYAN="\033[36m"
 GREEN="\033[32m"
 
+mkdir -p results
+
 printf "${RED}Running War Machine${COLOR_STOP}"
 echo
 sh ./libft-war-machine/grademe.sh | pv -s 8571 -w 80 > ./results/war-machine-results.txt 
 printf "${GREEN}Test done!${COLOR_STOP}"
 echo "Cleaning..."
 (cd .. && make fclean > /dev/null)
+
 printf "${RED}Running libf-unit-test:${COLOR_STOP}"
 echo
 (cd ./libft-unit-test && make f) | pv -s 13457 -w 80 > ./results/unit-test-results.txt
@@ -20,6 +23,7 @@ printf "${GREEN}Test done!${COLOR_STOP}"
 echo "Cleaning..."
 rm ../libft.so
 (cd .. && make fclean > /dev/null)
+
 printf "${UNDERLINE}${RED}Running Tripouille:${COLOR_STOP}"
 echo
 (cd ./libftTester/ && make a) | pv -s 8273 -w 80 > ./results/tripouille-results.txt
